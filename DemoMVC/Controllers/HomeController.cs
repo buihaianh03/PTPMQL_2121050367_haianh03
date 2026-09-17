f2011608de2b71a8a132e5d85f67ccfa9c71cae5
buihaianh03 <haianhxd123@gmail.com>
2026-09-17 07:32:00 +0700

    Tao cac trang Trang chu San pham va Lien he lesson 5

--- DemoMVC/Controllers/HomeController.cs
+++ DemoMVC/Controllers/HomeController.cs
@@ -21,4 +21,13 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+    public IActionResult Products()
+   {
+    return View();
+   }
+
+   public IActionResult Contact()
+  {
+    return View();
+  }
 }
